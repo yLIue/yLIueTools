d853450b972c10787a3aeacea2b44739afc6c705
yLIue <2138347243@qq.com>
2025-11-15 15:33:55 +0800

    '增加了调用品红颜色的函数'

--- yliuetools/Color.py
+++ yliuetools/Color.py
@@ -2,16 +2,20 @@ import os
 
 
 class Color(object):
-    MAGENTA = '\033[35m' # 品红
-    PURPLE = '\033[38;5;93m' # 256色
-    GREY = '\033[90m' # 灰色 亮黑
-    GREEN = '\033[92m' # 绿色 亮绿
-    RED = '\033[91m' # 红色 亮红
-    BLUE = '\033[94m' # 蓝色 亮蓝
-    YELLOW = '\033[93m' # 黄色 亮黄
-    CYAN = '\033[96m' # 青色 亮青
-    
-    END = '\033[0m' # 结束标记
+    MAGENTA = '\033[35m'  # 品红
+    PURPLE = '\033[38;5;93m'  # 256色
+    GREY = '\033[90m'  # 灰色 亮黑
+    GREEN = '\033[92m'  # 绿色 亮绿
+    RED = '\033[91m'  # 红色 亮红
+    BLUE = '\033[94m'  # 蓝色 亮蓝
+    YELLOW = '\033[93m'  # 黄色 亮黄
+    CYAN = '\033[96m'  # 青色 亮青
+
+    END = '\033[0m'  # 结束标记
+
+    @staticmethod
+    def magenta(_msg: str) -> str:
+        return f'{Color.MAGENTA}{_msg}{Color.END}'
 
     @staticmethod
     def purple(_msg: str) -> str:
